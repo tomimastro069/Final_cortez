743eaef898322c98110849ce66c87c5d7f711405
tomimastro069 <tomimastro069@gmail.com>
2025-12-09 22:11:53 -0300

    mas cambios , otra recursion mas del backend original

--- Final2025Python/models/address.py
+++ Final2025Python/models/address.py
@@ -1,7 +1,3 @@
-"""
-Module for the AddressModel class.
-"""
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -9,13 +5,10 @@ from models.base_model import BaseModel
 
 
 class AddressModel(BaseModel):
-    """
-    AddressModel class with attributes and relationships.
-    """
     __tablename__ = 'addresses'
 
     street = Column(String, index=True)
     number = Column(String)
     city = Column(String)
-    client_id = Column(Integer, ForeignKey('clients.id_key'), index=True)
+    client_id = Column(Integer, ForeignKey('clients.id'), index=True)  
     client = relationship('ClientModel', back_populates='addresses', lazy='select')
