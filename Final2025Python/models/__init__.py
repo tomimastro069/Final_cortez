e951294a15618e4699d2064c613a914d8df0f90a
tomimastro069 <tomimastro069@gmail.com>
2026-01-14 12:44:47 -0300

    presentacion final para entregar

--- Final2025Python/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This is the __init__.py file for the models package.
-
-It imports all the models for easier access.
-"""
-
-# If these models are used in other files via this package,
-# you can ignore the 'unused import' warning.
-# Otherwise, remove the unused imports.
